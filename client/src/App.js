55a47b7a02b233f3e1ab87a7220b30915dea5b73
Lisa <lisaxiong213@gmail.com>
2025-05-06 16:29:57 -0400

    added pages and modified App.js accordingly

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { indigo, amber } from '@mui/material/colors'
+import { createTheme } from "@mui/material/styles";
 import './App.css';
+
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import TrendsPage from './pages/TrendsPage';
+
+export const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: amber,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/trends" element={<TrendsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
